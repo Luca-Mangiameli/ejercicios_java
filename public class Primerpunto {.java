c1dd4df757189df39879ae1e690735b02485fb0a
Luca-Mangiameli <135647406+Luca-Mangiameli@users.noreply.github.com>
2023-08-22 17:02:33 -0300

    Add files via upload

---
+++ public class Primerpunto {.java
@@ -0,0 +1,17 @@
+public class Primerpunto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int num = 0;
+		for(int i = 1; i <= 100; i++) 
+		{
+		System.out.println(i);
+		if(i % 2 == 0);
+			System.out.print("(par)");
+		}
+		if(i < 100) {
+		System.out.print(";");
+		}
+	}
+}
